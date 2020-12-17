9e324798194fe92e3f264e7a7aabbd82c5b90384
Antonio Lopez <36496887+antonio-lopez@users.noreply.github.com>
2020-12-16 22:43:32 -0800

    updated images

--- covid-tracker/src/App.js
+++ covid-tracker/src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { Cards, Chart, CountryPicker, GlobalMetrics, Map, BarChart } from './components'; // comes from the components/index.js export
 import styles from './App.module.css';
 import { fetchCountryDataForBar } from './api';
-// import sdf from './img/covid-s.png'
+import covid from './img/covid_virus.png'
+import mask from './img/mask_cover.png';
 
 // USING FUNCTIONAL COMPONENTS WITH HOOKS AND APP IS CLASS BASED
 class App extends React.Component {
@@ -21,18 +22,14 @@ class App extends React.Component {
 
         return (
             <div className={styles.container}>
+                <img src={mask} alt="mask title"></img>
                 <span className={styles.title}>COVID-19 Cases</span>
-
-                {/* <span className={styles.title}>
-                    <span><img src={sdf} alt="covid"></img>
-                    COVID-19 Cases
-                    </span>
-                </span> */}
 
                 <Cards />   {/* DISPLAYS CARDS WITH GLOBAL DATA */}
                 <span className={styles.cards} />
 
-                <h1>Timeline of Total Cases</h1>
+                <img src={covid} alt="covid logo"></img>
+                <h1>Timeline of Total Global Cases</h1>
                 <Chart />   {/* DISPLAYS A RESPONSIVE GRAPH WITH GLOBAL DATA */}
 
                 <br /><br /><h1>Global Metrics</h1>
--- covid-tracker/src/components/Cards/Cards.jsx
+++ covid-tracker/src/components/Cards/Cards.jsx
@@ -25,28 +25,28 @@ const Cards = () => {
             <Grid container spacing={3} justify="center">
                 <Grid item component={Card} xs={12} md={2} className={cx(styles.card, styles.confirmed)}>
                     <CardContent>
-                        <Typography color="textSecondary" gutterBottom>Confirmed</Typography>
+                        <Typography color="textSecondary" gutterBottom>Confirmed (Global)</Typography>
                         <Typography variant="h5"><b>{numeral(globalData.cases).format("0,0")}</b></Typography>
                         <Typography variant="body2">Number of confirmed cases of Covid-19</Typography>
                     </CardContent>
                 </Grid>
                 <Grid item component={Card} xs={12} md={2} className={cx(styles.card, styles.active)}>
                     <CardContent>
-                        <Typography color="textSecondary" gutterBottom>Active</Typography>
+                        <Typography color="textSecondary" gutterBottom>Active (Global)</Typography>
                         <Typography variant="h5"><b>{numeral(globalData.active).format("0,0")}</b></Typography>
                         <Typography variant="body2">Number of active cases of Covid-19</Typography>
                     </CardContent>
                 </Grid>
                 <Grid item component={Card} xs={12} md={2} className={cx(styles.card, styles.recovered)}>
                     <CardContent>
-                        <Typography color="textSecondary" gutterBottom>Recovered</Typography>
+                        <Typography color="textSecondary" gutterBottom>Recovered (Global)</Typography>
                         <Typography variant="h5"><b>{numeral(globalData.recovered).format("0,0")}</b></Typography>
                         <Typography variant="body2">Number of recoveries from Covid-19</Typography>
                     </CardContent>
                 </Grid>
                 <Grid item component={Card} xs={12} md={2} className={cx(styles.card, styles.deaths)}>
                     <CardContent>
-                        <Typography color="textSecondary" gutterBottom>Deaths</Typography>
+                        <Typography color="textSecondary" gutterBottom>Deaths (Global)</Typography>
                         <Typography variant="h5"><b>{numeral(globalData.deaths).format("0,0")}</b></Typography>
                         <Typography variant="body2">Number of deaths by Covid-19</Typography>
                     </CardContent>
